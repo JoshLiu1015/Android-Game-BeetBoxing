ef6eedf6b933823c17a45894d177379cecf46f37
JoshLiu1015 <yhnujm36@gmail.com>
2023-11-13 16:58:42 -0600

    implemented life logic, increase falling speed as time goes

--- app/src/main/java/com/cs407/beet_boxing/MainActivity.java
+++ app/src/main/java/com/cs407/beet_boxing/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Context context;
 
-    @Override
+    @Override   
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
