8874f252e6a6b8be35a3e13f00d50a6f4b827aaa
Ronald Inglett <71849533+inglettronald@users.noreply.github.com>
2023-11-13 17:19:52 -0600

    implement layout for result screen

---
+++ app/src/main/java/com/cs407/beet_boxing/ResultsActivity.java
@@ -0,0 +1,13 @@
+package com.cs407.beet_boxing;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+}
